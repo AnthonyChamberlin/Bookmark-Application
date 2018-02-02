8350e54c2e7aee1d753091b4900ded43703a7616
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-02-02 21:21:24 +0000

    Take Values from inputs, save as object and stringify to localStorage

--- js/main.js
+++ js/main.js
@@ -3,7 +3,30 @@ document.getElementById('bookmarkForm').addEventListener('submit', saveBookmark)
 
 // Save Bookmark
 function saveBookmark(e) {
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteURL').value;
     
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    if(localStorage.getItem('bookmarks') === null) {
+        var bookmarks = [];
+
+        bookmarks.push(bookmark);
+
+        localStorage.setItem('bookmarks', JSON.stringify(bookmark));
+        console.log('Created first save!');
+    } else {
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+        bookmarks.push(bookmark);
+
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+        console.log('Added to Local Storage!');
+    }
 
     e.preventDefault();
 }
