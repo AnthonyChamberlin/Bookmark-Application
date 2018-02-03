2cb87ab3194c26608aab4e2b066520f9e28f589b
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-02-03 17:15:49 +0000

    Create function to fetch bookmarks, parse to JSON and output to document

--- js/main.js
+++ js/main.js
@@ -13,20 +13,36 @@ function saveBookmark(e) {
 
     if(localStorage.getItem('bookmarks') === null) {
         var bookmarks = [];
-
         bookmarks.push(bookmark);
-
-        localStorage.setItem('bookmarks', JSON.stringify(bookmark));
-        console.log('Created first save!');
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     } else {
         var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-
         bookmarks.push(bookmark);
-
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-
-        console.log('Added to Local Storage!');
     }
 
     e.preventDefault();
+}
+
+function fetchBookmarks() {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    var bookmarksResult = document.getElementById('bookmarksResult');
+
+    bookmarksResult.innerHTML = ' ';
+
+    for(var i = 0; i < bookmarks.length; i++) {
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResult.innerHTML += '<div class="well">' + 
+                                     '<h3>' + name + 
+                                     '<a class="btn btn-primary" target="_blank" href="'+ url +'">Visit</a>' + '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" target="_blank" href="#">Delete</a>'
+                                     '</h3>'
+                                     + '</div>';
+    }
+}
+
+function deleteBookmark() {
+    
 }
